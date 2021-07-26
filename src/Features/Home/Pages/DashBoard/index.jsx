c46ee2e5d61ac2c88587a7cc77c94692edf7b160
Sonnguyen <sonnguyen.pk1301@gmail.com>
2021-07-26 14:06:42 +0700

    edit map - change map souce - add chart component

--- src/Features/Home/Pages/DashBoard/index.jsx
+++ src/Features/Home/Pages/DashBoard/index.jsx
@@ -1,36 +1,71 @@
+import { Grid } from "@material-ui/core";
 import { covidApi } from "Api/covidApi";
 import MainLayout from "Components/Layouts";
-import Loading from "Components/Loading";
-import WorldCovidMap from "Features/Home/Components/Map";
+import PieChart from "Features/Home/Components/Charts/pieChart";
 import React, { useEffect, useState } from "react";
-import { addCasesToGEOJson } from "Utilise/utilise";
+import {
+  addCasesToGEOJson,
+  filterContinentData,
+  transformToMapData,
+} from "Utilise/utilise";
+import WorldMap from "Features/Home/Components/Map";
 
 HomePage.propTypes = {};
 
 function HomePage(props) {
-  const [covidData, setCovidData] = useState();
+  const [countriesData, setCountriesData] = useState();
+  const [continentsData, setContinentsData] = useState();
+  const [data, setData] = useState();
   const [isLoading, setIsLoading] = useState(false);
   const [filters, setFilters] = useState({ lastdays: 30 });
+  const [isGetNewestData, setIsGetNewsData] = useState(false);
 
   useEffect(() => {
-    (async () => {
-      try {
-        setIsLoading(true);
-        const data = await covidApi.getAllCountry();
-        const newData = addCasesToGEOJson(data);
-        setCovidData(newData);
-        setIsLoading(false);
-      } catch (error) {
-        console.log({ error });
-      }
-    })();
-  }, []);
+    setIsLoading(true);
+    getAllCountryData();
+    getContinentsData();
+    setIsLoading(false);
+  }, [isGetNewestData]);
+
+  const getAllCountryData = async () => {
+    try {
+      const data = await covidApi.getAllCountry();
+      const dataHighChart = transformToMapData(data);
+      setData(dataHighChart);
+    } catch (error) {
+      console.log({ error });
+    }
+  };
+
+  const getContinentsData = async () => {
+    try {
+      const continentsData = await covidApi.getAllContinent();
+      setContinentsData(filterContinentData(continentsData));
+    } catch (error) {
+      console.log({ error });
+    }
+  };
+
+  const handleUpdateNewData = () => {
+    if (isGetNewestData) {
+      setIsGetNewsData(false);
+    }
+    if (!isGetNewestData) {
+      setIsGetNewsData(true);
+    }
+  };
 
   return (
     <div>
       <MainLayout>
-        {isLoading && <Loading />}
-        {covidData && <WorldCovidMap dataCovid={covidData} />}
+        {data && <WorldMap countriesData={data} />}
+        <Grid container>
+          <Grid item xs={12} sm={8}>
+            {continentsData && <PieChart continentsData={continentsData} />}
+          </Grid>
+
+          <Grid item xs={12}></Grid>
+        </Grid>
       </MainLayout>
     </div>
   );
--- src/index.js
+++ src/index.js
@@ -8,7 +8,6 @@ import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import store from "Redux/store";
 import { SnackbarProvider } from "notistack";
-import "leaflet/dist/leaflet.css";
 
 ReactDOM.render(
   <React.StrictMode>
